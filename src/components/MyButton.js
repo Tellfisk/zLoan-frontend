ddacceb5e4b6e784703184bcb08e9d3536a7fbde
Tellfisk <tellev@outlook.com>
2021-04-19 14:06:42 +0200

    Update MyButton.js

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -8,14 +8,14 @@ export default class MyButton extends React.Component {
         this.notSelected = "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded";
         this.selected = "bg-blue-500  font-semibold text-white py-2 px-4 border border-transparent rounded";
     }
-    
+
     state = { isSelected: false }
 
+    // Switch from outline to solid background button
     switchAppearance = () => {
         this.setState({ isSelected: !this.state.isSelected })
     }
 
-    //I do not like this
     onClick = () => {
         this.props.whenPressed(); 
         this.switchAppearance();
